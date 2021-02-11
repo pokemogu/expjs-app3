ee57ae0fbc32becde90ce76cbc91ab8d0f4fb64d
YAMAHATA Kenichiro <pokemogu@gmail.com>
2021-02-11 17:54:53 +0900

    DB接続をPool方式に変更

--- index.js
+++ index.js
@@ -15,29 +15,38 @@ const port = process.env.NODE_PORT || 3000;
 app.set('view engine', 'ejs');
 
 // MySQL接続初期化
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const db_setting = {
   host: 'mysql_5.6.50',
   user: 'root',
-  password: process.env.ROOT_PASSWORD
+  password: process.env.ROOT_PASSWORD,
+  waitForConnections: true
 };
 
+const db = mysql.createPool(db_setting);
+
 // データベースとテーブルの作成
-(async () => {
-  const db = await mysql.createConnection(db_setting);
-  await db.execute("CREATE DATABASE IF NOT EXISTS database1");
-  try {
-    await db.execute("SELECT 1 FROM database1.table1 LIMIT 1");
-  }
-  catch (error) {
-    if (error.code == 'ER_NO_SUCH_TABLE')
-      await db.execute("CREATE TABLE database1.table1(id INTEGER AUTO_INCREMENT PRIMARY KEY, value VARCHAR(10))");
-    else
-      throw error;
-  }
-  await db.execute("INSERT INTO database1.table1(value) VALUES('hogehoge')");
-  db.end();
-})(db_setting).catch();
+db.getConnection((err, conn) => {
+  conn.query("CREATE DATABASE IF NOT EXISTS database1", (err, results, fields) => {
+    if (err)
+      throw err;
+    conn.query("SELECT 1 FROM database1.table1 LIMIT 1", (err, results, fields) => {
+      if (err)
+        if (err.code == 'ER_NO_SUCH_TABLE') {
+          conn.query("CREATE TABLE database1.table1(id INTEGER AUTO_INCREMENT PRIMARY KEY, value VARCHAR(10))", (err, results, fields) => {
+            if (err)
+              throw err;
+            conn.query("INSERT INTO database1.table1(value) VALUES('hogehoge')");
+            db.releaseConnection(conn);
+          });
+        }
+        else
+          throw err;
+    });
+  });
+});
+app.locals.db = db;
+
 
 // *****************************************
 // ルーティング
@@ -45,16 +54,23 @@ const db_setting = {
 
 // ルート
 app.get('/', (req, res, next) => {
-  (async (req, res, next) => {
-    const db = await mysql.createConnection(db_setting);
-    const [results, fields] = await db.execute('SELECT * FROM database1.table1');
+
+  const db = req.app.locals.db;
+  if (!db) {
+    res.set('Content-Type', 'text/plain');
+    res.send('DBに接続されていません。');
+    next();
+    return;
+  }
+  db.query('SELECT * FROM database1.table1', (err, results, fields) => {
     let text = '';
     for (const result of results) {
       text += result.id + ' -> ' + result.value + "\n";
     }
     res.set('Content-Type', 'text/plain');
     res.send(text);
-  })(req, res, next).catch(next);
+    next();
+  });
 });
 
 // *****************************************
