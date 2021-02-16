b5b7d4b0b3cf73ec4f1e14ae03152ff7de5d3b51
YAMAHATA Kenichiro <pokemogu@gmail.com>
2021-02-16 22:42:16 +0900

    mysql2/promiseを使用した同期処理に変更

--- index.js
+++ index.js
@@ -15,84 +15,77 @@ const port = process.env.NODE_PORT || 3000;
 app.set('view engine', 'ejs');
 
 // MySQL接続初期化
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const db_setting = {
   host: 'mysql_5.6.50',
   user: 'root',
-  password: process.env.ROOT_PASSWORD,
-  connectTimeout: 5000,
-  waitForConnections: true
+  password: process.env.ROOT_PASSWORD
 };
+app.locals.db_setting = db_setting;
 
-const db = mysql.createPool(db_setting);
 let retry_count = 5;
 
 // データベースとテーブルの作成
-const attemptConnection = () => {
-  db.getConnection((err, conn) => {
-    if (err) {
-      if (retry_count > 0) {
-        setTimeout(attemptConnection, db_setting.connectTimeout);
-        console.log('error connecting. retrying in ' + (db_setting.connectTimeout / 1000) + ' sec');
-        retry_count--;
-      }
-      else
-        throw err;
+const attemptConnection = async () => {
+  let conn;
+  try {
+    conn = await mysql.createConnection(db_setting);
+    console.log('DB connection started.');
+  } catch (err) {
+    if (retry_count > 0) {
+      setTimeout(attemptConnection, 5000);
+      console.log('DB connection retrying...');
+      retry_count--;
+      return;
     }
-    else {
-      conn.query("CREATE DATABASE IF NOT EXISTS database1", (err, results, fields) => {
-        if (err)
-          throw err;
-        else {
-          conn.query("SELECT 1 FROM database1.table1 LIMIT 1", (err, results, fields) => {
-            if (err)
-              if (err.code == 'ER_NO_SUCH_TABLE') {
-                conn.query("CREATE TABLE database1.table1(id INTEGER AUTO_INCREMENT PRIMARY KEY, value VARCHAR(10))", (err, results, fields) => {
-                  if (err)
-                    throw err;
-                  conn.query("INSERT INTO database1.table1(value) VALUES('hogehoge')");
-                  db.releaseConnection(conn);
-                });
-              }
-              else
-                throw err;
-          });
+    else
+      throw err;
+  }
+
+  try {
+    await conn.query('CREATE DATABASE IF NOT EXISTS database1');
+    await conn.query('CREATE TABLE IF NOT EXISTS database1.table1(id INTEGER AUTO_INCREMENT PRIMARY KEY, value VARCHAR(10))');
+    await conn.query("INSERT INTO database1.table1(value) VALUES('hogehoge')");
+  } catch (err) {
+    throw err;
+  } finally {
+    conn.end();
+  }
+  console.log('DB initializing completed.');
+
+  // *****************************************
+  // ルーティング
+  // *****************************************
+
+  // ルート
+  app.get('/', (req, res, next) => {
+    const db_setting = req.app.locals.db_setting;
+
+    (async () => {
+      const conn = await mysql.createConnection(db_setting);
+      try {
+        const [results, fileds] = await conn.query('SELECT * FROM database1.table1');
+        let text = '';
+        for (const result of results) {
+          text += result.id + ' -> ' + result.value + "\n";
         }
-      });
-    }
+        res.set('Content-Type', 'text/plain');
+        res.send(text);
+        next();
+      } catch (err) {
+        throw err;
+      } finally {
+        conn.end();
+      }
+    })();
+  });
+
+  // *****************************************
+  // サーバー開始
+  // *****************************************
+  app.listen(port, () => {
+    console.log('Server started');
   });
 }
 attemptConnection();
 
-app.locals.db = db;
-
-
-// *****************************************
-// ルーティング
-// *****************************************
-
-// ルート
-app.get('/', (req, res, next) => {
-
-  const db = req.app.locals.db;
-  db.getConnection((err, conn) => {
-    if (err)
-      throw err;
-    conn.query('SELECT * FROM database1.table1', (err, results, fields) => {
-      let text = '';
-      for (const result of results) {
-        text += result.id + ' -> ' + result.value + "\n";
-      }
-      res.set('Content-Type', 'text/plain');
-      res.send(text);
-      next();
-    });
-  });
-});
-
-// *****************************************
-// サーバー開始
-// *****************************************
-app.listen(port, () => {
-  console.log('Server started');
-});
